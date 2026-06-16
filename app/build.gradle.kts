04439b7261358680200e3ba3043ce240be6d8f60
palinkiewicz <pogromca.ap@gmail.com>
2026-06-16 18:39:00 +0200

    Update app's version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "pl.dakil.music"
         minSdk = 29
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.2.0"
+        versionCode = 4
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
